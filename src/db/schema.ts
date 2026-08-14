f55a492203c8fbeb2e6615440d06c0284d572121
Jirasak-Thongpila <tle0613828245@gmail.com>
2026-08-14 21:42:17 +0700

    feat: replace google_id with password column in users schema and add db:push script

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,9 +2,9 @@ import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
-  googleId: text("google_id").notNull().unique(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
+  password: text("password").notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
